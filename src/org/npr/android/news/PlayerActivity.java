8d2d26115108a1428cb6a83851e1175cdaf720aa
mfrederick@google.com <mfrederick@google.com@0a442a4a-40cc-11de-998b-8fedbbe774c7>
2010-08-17 17:31:36 +0000

    Proposed base class for all activities (issue1941045)
    git-svn-id: http://npr-android-app.googlecode.com/svn/trunk/Npr@92 0a442a4a-40cc-11de-998b-8fedbbe774c7

---
+++ src/org/npr/android/news/PlayerActivity.java
@@ -0,0 +1,136 @@
+// Copyright 2010 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.npr.android.news;
+
+import android.app.Activity;
+import android.app.ActivityGroup;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TextView;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @author mfrederick@google.com (Michael Frederick)
+ *
+ * A base class for all Activities that want to display the default layout,
+ * including the ListenView. 
+ */
+/**
+ * @author mfrederick@google.com (Your Name Here)
+ *
+ */
+public abstract class PlayerActivity extends ActivityGroup implements
+    Trackable, Refreshable {
+  private TextView titleText;
+  public abstract CharSequence getMainTitle();
+  private static final String LOG_TAG = PlayerActivity.class.getName();
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Override the normal volume controls so that the user can alter the volume
+    // when a stream is not playing.
+    setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+    setContentView(R.layout.new_main);
+    titleText = (TextView) findViewById(R.id.LogoNavText);
+    titleText.setText(getMainTitle());
+
+    // TODO(mfrederick): Re-enable after ListenView is ready.
+//    ListenView lv = new ListenView(this);
+//    ((ViewGroup) findViewById(R.id.MediaPlayer)).addView(lv,
+//        new ViewGroup.LayoutParams(LayoutParams.FILL_PARENT,
+//            LayoutParams.WRAP_CONTENT));
+  }
+
+  @Override
+  public boolean isRefreshable() {
+    return false;
+  }
+
+  @Override
+  public void refresh() {
+  }
+
+  @Override
+  public void trackNow() {
+  }
+
+  @Override
+  public void finish() {
+    super.finish();
+    noAnimation();
+  }
+
+  /**
+   * Prevents the default animation on the pending transition. Only works on
+   * SDK version 5 and up, but may be safely called from any version.
+   */
+  public void noAnimation() {
+    try {
+      Method overridePendingTransition =
+          Activity.class.getMethod("overridePendingTransition", new Class[] {
+              int.class, int.class});
+      overridePendingTransition.invoke(this, 0, 0);
+    } catch (SecurityException e) {
+      Log.e(LOG_TAG, "", e);
+    } catch (NoSuchMethodException e) {
+      Log.e(LOG_TAG, "", e);
+    } catch (IllegalArgumentException e) {
+      Log.e(LOG_TAG, "", e);
+    } catch (IllegalAccessException e) {
+      Log.e(LOG_TAG, "", e);
+    } catch (InvocationTargetException e) {
+      Log.e(LOG_TAG, "", e);
+    }
+  }
+
+  private enum MenuId {
+    ABOUT,
+    REFRESH
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    menu.add(Menu.NONE, MenuId.ABOUT.ordinal(), Menu.NONE,
+      R.string.msg_main_menu_about)
+      .setIcon(android.R.drawable.ic_menu_help).setAlphabeticShortcut('a');
+    if (this.isRefreshable()) {
+      menu.add(Menu.NONE, MenuId.REFRESH.ordinal(), Menu.NONE,
+        R.string.msg_refresh).setAlphabeticShortcut('r');
+    }
+    return (super.onCreateOptionsMenu(menu));
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.getItemId() == MenuId.ABOUT.ordinal()) {
+      startActivity(new Intent(this, AboutActivity.class));
+      return true;
+    } else if (item.getItemId() == MenuId.REFRESH.ordinal()) {
+      this.refresh();
+    }
+    return super.onOptionsItemSelected(item);
+  }
+}
